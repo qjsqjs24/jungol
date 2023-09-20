271dc47784f3c2be30fd42e4fba65364579d8b03
21-manic-020g <21-manic-020g@21-manic-020>
2023-09-20 12:29:57 +0900

    510 문제 풀이

---
+++ Jungol/src/q510/Main.java
@@ -0,0 +1,14 @@
+package q510;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		num1 = -1;
+		num2 = 100;
+		System.out.println(num1);
+		System.out.println(num2);
+
+	}
+
+}
