89ac7010bd78f3f1abdc09558e8afae644380b74
21-manic-020g <21-manic-020g@21-manic-020>
2023-10-05 17:46:41 +0900

    풀이

---
+++ Jungol/src/q9019/Main.java
@@ -0,0 +1,30 @@
+package q9019;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		sc.close();
+		int sum1 = 10 + 3;
+		int sum2 = 5 - 3;
+		int sum3 = 7 * 3;
+		int sum4 = 9 / 3;
+		int sum5 = 10 % 3;
+		System.out.print("5개의 수를 입력하시오. ");
+		System.out.print(sum1 + " ");
+		System.out.print(sum2 + " ");
+		System.out.print(sum3 + " ");
+		System.out.print(sum4 + " ");
+		System.out.print(sum5);
+		
+
+	}
+
+}
